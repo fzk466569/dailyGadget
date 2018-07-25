ce3a97f2207c618bb190ad0fb5935a55d37b31db
fzk466569@163.com <fzk466569@163.com>
2018-07-25 11:09:36 +0800

    upload easy decorator

---
+++ test_decorator/time_it.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: fzk
+# @Time  14:39
+from functools import wraps
+import time
+
+
+def clock(f):
+    @wraps(f)
+    def clockd(*args, **kwargs):
+        start = time.time()
+        result = f(*args, **kwargs)
+        print('f.__name__: ', f.__name__)
+        end = time.time()
+        print('func {0} 耗时 {1} seconds'.format(f.__name__, end - start))
+        return result
+
+    return clockd
+
+
+@clock
+def test(times):
+    time.sleep(times)
+    return 'sssssssss'
+
+
+if __name__ == '__main__':
+    times = 1
+    print test(times)
+    print(test.func_name)
+    print(test.__name__)
+
+    # ('f.__name__: ', 'test')
+    # func test 耗时 1.00527596474 seconds
+    # sssssssss
+    # test
+    # test

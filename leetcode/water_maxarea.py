a6ca10c29d119a5aa7bbf3dc8ce9f0145db31921
fzk466569@163.com <fzk466569@163.com>
2018-10-30 16:41:55 +0800

    update

---
+++ leetcode/water_maxarea.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: fzk
+# @Time  16:40
+
+
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        maxarea,l,r = 0, 0, len(height) - 1
+        while (l < r):
+            maxarea = max(maxarea, min(height[l], height[r]) * (r-l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return maxarea
